37f4352e36f6f8cb2a66fbdb3aa84e807ec974c2
Fran Rovaretti <franciscor1905@gmail.com>
2022-11-02 10:55:06 -0300

    2/11

---
+++ js/main.js
@@ -0,0 +1,220 @@
+// ==============Obj abonos=================
+const listaAbonos = [
+    {
+        id : 1,
+        nombre : "6MB",
+        precio : 2990,
+        liCaracteristicas : ["Cargo de instalación $9500","No requiere servicios adicionales","Consultar disponibilidad en la zona"]
+    },
+    {
+        id : 2,
+        nombre : "10MB",
+        precio : 3940,
+        liCaracteristicas : ["Cargo de instalación $9500","No requiere servicios adicionales","Consultar disponibilidad en la zona"]
+    },
+    {
+        id : 3,
+        nombre : "15MB",
+        precio : 4190,
+        liCaracteristicas : ["Cargo de instalación $9500","No requiere servicios adicionales","Consultar disponibilidad en la zona"]
+    }
+]
+
+
+// =============Arrays y variables=================
+
+
+let listaNombresAbonos = [];
+for (const abono of listaAbonos){
+    listaNombresAbonos.push(abono.nombre)
+} 
+
+let listaPreciosAbonos = [];
+
+for (const abono of listaAbonos){
+    listaPreciosAbonos.push(abono.precio)
+} 
+let listaNombresClientes = []
+let abonoElegido=[] ;
+let precioInstalacion = 9500;
+
+// ================Elegir abono==================
+
+const formulario =document.getElementById("formulario") ;
+const serviciosResidenciales = document.getElementById("servicios-residenciales");
+const reset = document.getElementById("btn-reset");
+
+for(const abono of listaAbonos){
+    let card = document.createElement ("div");
+    card.className = "card-servicio";
+    card.innerHTML = `<p class="titulo-card">${abono.nombre}</p>
+    <ul class="lista-servicios">
+        <li class="item-lista-internet">${abono.liCaracteristicas[0]}</li>
+        <li class="item-lista-internet">${abono.liCaracteristicas[1]}</li>
+        <li class="item-lista-internet">${abono.liCaracteristicas[2]}</li>
+    </ul> 
+    <p><b>$${abono.precio}</b></p>
+    <button =onclick id="${abono.id}" class="btn btn-success">Elegir abono</button>`;
+    serviciosResidenciales.append (card)
+    const boton = document.getElementById(`${abono.id}`);
+    boton.addEventListener("click",()=>{
+        elegirAbono(abono.id);
+        mostrarFormulario ();
+        
+        
+    })
+
+} ;
+// ===============================================================
+
+let elegirAbono = (abonoId) => {
+const item = listaAbonos.find((abono) => abono.id === abonoId);
+    abonoElegido.push(item);
+};
+
+// =============formulario=====================
+
+function storageForm(){
+    document.addEventListener ('DOMContentLoaded',  ()=> {
+
+        if(localStorage.getItem('cliente')){
+            cliente = JSON.parse(localStorage.getItem('cliente'));
+            abonoElegido = JSON.parse(localStorage.getItem('abonoElegido'))
+        }
+    })
+    localStorage.setItem('cliente', JSON.stringify(cliente))
+    localStorage.setItem('abonoElegido', JSON.stringify(abonoElegido))
+    mostrarMensajeBienvenida()
+};
+function mostrarMensajeBienvenida (){
+    for (const abonoe of abonoElegido){
+        let nombreAbonoe = abonoe.nombre
+        let precioAbonoe = abonoe.precio
+    
+    
+    serviciosResidenciales.innerHTML =  `
+    <div>
+    <p><b>Bienvenido ${cliente[0]} a Red Metropolitana</b></p> 
+    <p>Abono elegido: <b>${nombreAbonoe}</b></p>
+    <p>total : $${precioAbonoe + precioInstalacion}</p>
+    </div>
+    `
+    }
+}
+
+function mostrarFormulario (){
+    serviciosResidenciales.innerHTML = 
+    `<div class="container">
+        <div class="row">
+            <div class="col-md-12 formu">
+                <div class="well well-sm">
+                    <form class="form-horizontal" method="get">
+                        <fieldset>
+                            <legend class="text-center header">Ingrese los siguientes datos para confirmar la instalacion:</legend>
+
+                            <div class="form-group">
+                                <span class="col-md-1 col-md-offset-2 text-center"><i
+                                        class="fa fa-user bigicon"></i></span>
+                                <div class="col-md-8">
+                                    <input id="fname" name="name" type="text" placeholder="Nombre"
+                                        class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <span class="col-md-1 col-md-offset-2 text-center"><i
+                                        class="fa fa-user bigicon"></i></span>
+                                <div class="col-md-8">
+                                    <input id="lname" name="name" type="text" placeholder="Apellido"
+                                        class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <span class="col-md-1 col-md-offset-2 text-center"><i
+                                        class="fa fa-user bigicon"></i></span>
+                                <div class="col-md-8">
+                                    <input id="dni" name="phone" type="text" placeholder="DNI"
+                                        class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i
+                                    class="fa fa-pencil-square-o bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="direccion" name="phone" type="text" placeholder="Direccion"
+                                    class="form-control">
+                            </div>
+                        </div>
+
+                            <div class="form-group">
+                                <span class="col-md-1 col-md-offset-2 text-center"><i
+                                        class="fa fa-envelope-o bigicon"></i></span>
+                                <div class="col-md-8">
+                                    <input id="email" name="email" type="text" placeholder="Email "
+                                        class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <span class="col-md-1 col-md-offset-2 text-center"><i
+                                        class="fa fa-phone-square bigicon"></i></span>
+                                <div class="col-md-8">
+                                    <input id="phone" name="phone" type="text" placeholder="Telefono"
+                                        class="form-control">
+                                </div>
+                            </div>
+                           
+                
+                            <div class="form-group">
+                                <span class="col-md-1 col-md-offset-2 text-center"><i
+                                        class="fa fa-pencil-square-o bigicon"></i></span>
+                                <div class="col-md-8">
+                                    <textarea class="form-control" id="message" name="message"
+                                        placeholder="Referencias del domicilio."
+                                        rows="7"></textarea>
+                                </div>
+                            </div>
+
+                        
+
+                            <div class="form-group">
+                                <div class="col-md-12 text-center contenedor-btn">
+                                    <button type="submit" id= "btn-enviar" class="btn btn-primary btn-lg">Enviar</button>
+                                    <button type="reset" id= "btn-reset" class="btn btn-primary btn-lg">Reset</button>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>`
+    capturar();
+    const enviar = document.getElementById("btn-enviar");
+    enviar.addEventListener("click",storageForm);
+    
+};
+
+function capturar(){    
+    const nombre =document.getElementById("fname") 
+    const apellido =document.getElementById("lname") 
+    const dni =document.getElementById("dni")
+    const direccion =document.getElementById("direccion") 
+    const eMail =document.getElementById("email")
+    const telefono =document.getElementById("phone")
+    const ref =document.getElementById("message")
+
+    nombre.addEventListener('change',() => cliente.push(nombre.value))
+    apellido.addEventListener('change',() => cliente.push(apellido.value))
+    dni.addEventListener('change',() => cliente.push(dni.value))
+    direccion.addEventListener('change',() => cliente.push(direccion.value))
+    eMail.addEventListener('change',() => cliente.push(eMail.value))
+    telefono.addEventListener('change',() => cliente.push(telefono.value))
+    ref.addEventListener('change',() => cliente.push(ref.value))
+
+    
+};
+
+let cliente = [ ];
+console.log(cliente)
---
+++ js/producto.js
@@ -0,0 +1,105 @@
+// let precioTotal = 0;
+function Producto (id,nombre,stock,precio,img,liCaracteristicas,cantidad){
+    this.id = id
+    this.nombre = nombre;
+    this.stock = stock || 0;
+    this.precio = precio;
+    this.img = img;
+    this.liCaracteristicas = liCaracteristicas;
+    this.cantidad = cantidad
+}
+let producto1 = new Producto (1,"TL-WR820N",10, 3550,"../img/820n2-300x300.jpg",["Velocidad de transmisión Inalámbrica de 300Mbps.","IPTV es compatible con los nuevos protocolos para optimizar la transmisión","Los protocolos de seguridad protegen su red doméstica con firewalls y encriptación inalámbrica."],1);
+let producto2 = new Producto (2,"ARCHER C20",10, 6999,"../img/archerc202-300x300.jpg",["Soporta el estándar 802.11ac.","Conexiones simultáneas de 2.4GHz y 5GHz","Señal omnidireccional estable."],1);
+let producto3 = new Producto (3,"ARCHER C60",5, 17600,"../img/archerc602-300x300.jpg", ["Consigue un Wi-Fi más rápido","El avanzado Wi-Fi AC desbloquea el rendimiento de tus dispositivos inalámbricos","Funcionalidades avanzadas de software como Control Parental y Red de Invitados"],1);
+
+let listaProductos = [producto1, producto2, producto3 ];
+let listaProductosCStock = listaProductos.filter((producto) => producto.stock > 0) ;
+console.log (listaProductos)
+// ========================================================================================
+
+
+const contenedorCarrito = document.getElementById("contenedor-carrito")
+
+const precioTotal = document.getElementById ("precio-total")
+
+let carrito = [];
+
+document.addEventListener ('DOMContentLoaded',  ()=> {
+    if(localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'));
+        actualizarCarrito()
+    }
+
+})
+
+
+// =====================carrito========================
+
+let catalogo = document.getElementById("catalogo")
+
+for(const producto of listaProductosCStock){
+    let card = document.createElement ("div");
+    card.className = "card-servicio";
+    card.innerHTML = `<img src="${producto.img}"></img>
+    <p class="titulo-card">${producto.nombre}</p>
+    <ul class="lista-servicios">
+        <li class="item-lista-internet">${producto.liCaracteristicas[0]}</li>
+        <li class="item-lista-internet">${producto.liCaracteristicas[1]}</li>
+        <li class="item-lista-internet">${producto.liCaracteristicas[2]}</li>
+    </ul> 
+    <p><b>$${producto.precio}</b></p> 
+    <button id="agregar ${producto.id}"class="boton-contacto">Agregar al carrito</button> `;
+    
+    catalogo.appendChild(card);
+    const boton = document.getElementById(`agregar ${producto.id}`);
+    boton.addEventListener("click",()=>{
+        agregarAlCarrito(producto.id)
+
+    })
+}; 
+
+const agregarAlCarrito = (prodId) => {
+    const existe = carrito.some(prod => prod.id === prodId);
+    if(existe){
+        const prod = carrito.map(prod => {
+            if(prod.id === prodId){
+                prod.cantidad++
+                prod.precio*prod,cantidad
+            }
+        })
+    }
+    else{
+    const item = listaProductos.find((prod) => prod.id === prodId);
+    carrito.push(item );
+    
+    console.log(carrito);
+};
+actualizarCarrito();
+}
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId)
+    const indice = carrito.indexOf(item)
+    carrito.splice(indice,1)
+    actualizarCarrito();
+}
+
+
+const actualizarCarrito = ()=> {
+    contenedorCarrito.innerHTML = ``
+    carrito.forEach((prod) => {
+        const div = document.createElement('div')
+        div.className = ("producto-carrito")
+        div.innerHTML= 
+        `<img class = "carrito-img"src = ${prod.img}></img>
+        <p>${prod.nombre}</p>
+        <p>$${prod.precio}</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button class="eliminar boton-contacto" onclick = "eliminarDelCarrito(${prod.id})">Eliminar<i class="fa-solid fa-trash-xmark icon-carrito"></i></button>
+        `
+        
+        contenedorCarrito.appendChild(div)
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+        
+    })
+    precioTotal.innerText = carrito.reduce((acc,prod) => acc + prod.precio, 0 )
+}
